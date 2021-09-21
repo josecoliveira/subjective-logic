c067a4edf0371bcd5bc39cdecdaddb985d4b5449
José C. Oliveira <josecarlosdeoliveirajr@gmail.com>
2021-09-21 02:48:10 -0500

    Initial states and trust graph

--- SubjectiveLogic/Trust.py
+++ SubjectiveLogic/Trust.py
@@ -9,6 +9,6 @@ def trust_discount_2e(hab: HyperopinionInterface, hbx: HyperopinionInterface):
     if hab.k != 2:
         raise Exception("Trust opinion must have cardinality 2.")
     k = hbx.k
-    b = hab.P[0]* hbx.b
+    b = hab.P[0] * hbx.b
     a = hbx.a
     return Hyperopinion(k, b, a)
